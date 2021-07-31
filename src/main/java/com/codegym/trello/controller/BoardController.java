4a0b9919014d08af713535d6d2c807236c3b9401
thanhdamquoc <79950542+thanhdamquoc@users.noreply.github.com>
2021-07-31 11:38:12 +0700

    add CRUD for User and Board

---
+++ src/main/java/com/codegym/trello/controller/BoardController.java
@@ -0,0 +1,52 @@
+package com.codegym.trello.controller;
+
+import com.codegym.trello.model.Board;
+import com.codegym.trello.service.board.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/boards")
+public class BoardController {
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Board>> findAll() {
+        return new ResponseEntity<>(boardService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Board> findById(@PathVariable Long id) {
+        Optional<Board> boardOptional = boardService.findById(id);
+        if (!boardOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(boardOptional.get(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Board> add(@RequestBody Board board) {
+        return new ResponseEntity<>(boardService.save(board), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Board> update(@PathVariable Long id, @RequestBody Board board) {
+        board.setId(id);
+        return new ResponseEntity<>(boardService.save(board), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Board> deleteById(@PathVariable Long id) {
+        Optional<Board> boardOptional = boardService.findById(id);
+        if (!boardOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        boardService.deleteById(id);
+        return new ResponseEntity<>(boardOptional.get(), HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/com/codegym/trello/service/user/UserService.java
@@ -0,0 +1,7 @@
+package com.codegym.trello.service.user;
+
+import com.codegym.trello.model.User;
+import com.codegym.trello.service.GeneralService;
+
+public interface UserService extends GeneralService<User> {
+}
